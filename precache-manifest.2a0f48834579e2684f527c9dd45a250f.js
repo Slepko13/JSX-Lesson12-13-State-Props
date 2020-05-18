6e6f0468c206f5308ef2e0f3bd119b766764b05b
Mykola Slepko <slepko13@gmail.com>
2020-05-18 23:42:03 +0300

    Updates

---
+++ precache-manifest.2a0f48834579e2684f527c9dd45a250f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2a49b49a943ff25de258f7ec8ea2a8",
+    "url": "/JSX-Lesson12-13-State-Props/index.html"
+  },
+  {
+    "revision": "3b0b2df3ee761e6a5277",
+    "url": "/JSX-Lesson12-13-State-Props/static/css/main.171763ab.chunk.css"
+  },
+  {
+    "revision": "716f3392059d9a1a0ca7",
+    "url": "/JSX-Lesson12-13-State-Props/static/js/2.195aa298.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JSX-Lesson12-13-State-Props/static/js/2.195aa298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b0b2df3ee761e6a5277",
+    "url": "/JSX-Lesson12-13-State-Props/static/js/main.f22ea7a5.chunk.js"
+  },
+  {
+    "revision": "5f012b0c2db72de45bf0",
+    "url": "/JSX-Lesson12-13-State-Props/static/js/runtime-main.a2c1ce76.js"
+  }
+]);
